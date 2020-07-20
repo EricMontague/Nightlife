77d578b8d8b7a7c15008d1ed249074c6966bf596
Eric Montague <eric.g.montague@gmail.com>
2020-07-20 00:27:24 -0400

    added implementation for all of the sign in and registration methods as well as refactored out the Home component

--- src/components/authentication/AuthApp.js
+++ src/components/authentication/AuthApp.js
@@ -1,49 +1,99 @@
 import React from "react";
 import CenteredPageLayout from "../common/CenteredPageLayout";
 import Card from "../common/Card";
-import Home from "./Home";
 import SignInForm from "./SignInForm";
 import SignUpForm from "./SignUpForm";
-import { signInWithGoogle } from "../../services/firebase";
+import {
+  signInWithGoogle,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  storeUserDocument
+} from "../../services/firebase";
+import { UserContext } from "../../context/UserProvider";
+import { Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
 
 class AuthApp extends React.Component {
   constructor(props) {
     super(props);
     this.renderContet = this.renderContent.bind(this);
+    this.signInWithGoogleOAuth = this.signInWithGoogleOAuth.bind(this);
+  }
+
+  static contextType = UserContext;
+
+  async signInWithGoogleOAuth() {
+    try {
+      const result = await signInWithGoogle();
+      console.log("Sign in successful!");
+    } catch (error) {
+      console.log(`Sign in Error: ${error}`);
+    }
+  }
+
+  async registerUser(user) {
+    try {
+      const { newUser } = await createUserWithEmailAndPassword(
+        user.email,
+        user.password
+      );
+      storeUserDocument({
+        email: user.email,
+        password: user.password,
+        displayName: user.firstName + " " + user.lastName
+      });
+      console.log(`New User registered: ${newUsers}`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async loginUser(email, password) {
+    try {
+      const result = await signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   renderContent() {
-    switch (this.props.match.path) {
-      case "/signin":
-        return (
-          <>
-            <SignInForm signInWithGoogle={signInWithGoogle} />
-          </>
-        );
-
-      case "/signup":
-        return (
-          <>
-            <SignUpForm signInWithGoogle={signInWithGoogle} />
-          </>
-        );
-
-      default:
-        return (
-          <>
-            <Home />
-          </>
-        );
+    const path = this.props.match.path;
+    if (path === "/signin") {
+      return (
+        <>
+          <SignInForm
+            signInWithGoogleOAuth={() => this.signInWithGoogleOAuth()}
+          />
+        </>
+      );
+    } else if (path === "/signup") {
+      return (
+        <>
+          <SignUpForm
+            signInWithGoogleOAuth={() => this.signInWithGoogleOAuth()}
+            createUserWithEmailAndPasswordHandler={(email, password) =>
+              this.registerUser(email, password)
+            }
+          />
+        </>
+      );
     }
   }
 
   render() {
-    return (
-      <CenteredPageLayout>
-        <Card classes="card-medium">{this.renderContent()}</Card>
-      </CenteredPageLayout>
-    );
+    if (this.context.isLoggedIn) {
+      return (
+        <Redirect
+          to={`/users/${this.context.currentUser.displayName.replace(" ", "")}`}
+        />
+      );
+    } else {
+      return (
+        <CenteredPageLayout>
+          <Card classes="card-medium">{this.renderContent()}</Card>
+        </CenteredPageLayout>
+      );
+    }
   }
 }
 
