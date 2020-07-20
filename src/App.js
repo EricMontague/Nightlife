2fdcd22de25b390d8b0218f54527534f113d5fcb
Eric Montague <eric.g.montague@gmail.com>
2020-07-20 00:26:38 -0400

    Added new routes to the app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/common/Navbar";
 import UserProvider from "./context/UserProvider";
 import DeviceProvider from "./context/DeviceProvider";
 import AuthApp from "./components/authentication/AuthApp";
+import Home from "./components/authentication/Home";
+import UserProfile from "./components/profile/UserProfile";
 import NotFound from "./components/common/NotFound";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -13,11 +15,15 @@ const App = () => {
         <Router>
           <Navbar />
           <Switch>
+            <Route exact path="/" render={() => <Home />} />
             <Route
               exact
-              path={["/", "/signin", "/signup"]}
-              render={({ match }) => <AuthApp match={match} />}
+              path={["/signin", "/signup"]}
+              render={({ match, history }) => (
+                <AuthApp match={match} history={history} />
+              )}
             />
+            <Route exact path="/users/:displayName" component={UserProfile} />
             <Route exact path="*" component={NotFound} />
           </Switch>
         </Router>
