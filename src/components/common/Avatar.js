a4652c9f84dd53e8fb97b717bd3a2740100bd815
Eric Montague <eric.g.montague@gmail.com>
2020-07-21 00:28:40 -0400

    added logic to add a default photo if a user doesn't have one

--- src/components/common/Avatar.js
+++ src/components/common/Avatar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import defaultUserPhoto from "../../assets/default_avatar.png";
 
 const Avatar = props => {
   return (
     <img
       className={props.avatarClasses}
-      src={props.user.photoURL}
+      src={props.user.photoURL ? props.user.photoURL : defaultUserPhoto}
       alt={props.user.displayName}
     />
   );
