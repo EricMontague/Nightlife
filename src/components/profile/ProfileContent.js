fc0a2d961266c615c3bfe89c0061234e23cc6eb6
Eric Montague <eric.g.montague@gmail.com>
2020-07-22 23:16:48 -0400

    fixed error with prop types.

--- src/components/profile/ProfileContent.js
+++ src/components/profile/ProfileContent.js
@@ -21,7 +21,7 @@ ProfileContent.propTypes = {
       id: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
       description: PropTypes.string.isRequired,
-      datetime: PropTypes.string.isRequired,
+      datetime: PropTypes.instanceOf(Date).isRequired,
       image: PropTypes.string.isRequired,
       placeIds: PropTypes.arrayOf(PropTypes.string.isRequired)
     })
