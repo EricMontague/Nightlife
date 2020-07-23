47c01e2c385641c6b2f3c3fff1b0d3c690f91060
Eric Montague <eric.g.montague@gmail.com>
2020-07-22 23:15:57 -0400

    Corrected error with prop types

--- src/components/profile/Plans.js
+++ src/components/profile/Plans.js
@@ -38,7 +38,7 @@ Plans.propTypes = {
       id: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
       description: PropTypes.string.isRequired,
-      datetime: PropTypes.string.isRequired,
+      datetime: PropTypes.instanceOf(Date).isRequired,
       image: PropTypes.string.isRequired,
       placeIds: PropTypes.arrayOf(PropTypes.string.isRequired)
     })
