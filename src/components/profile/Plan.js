78b2d5e958206a56d2d26e4d346e00072c4bb76a
Eric Montague <eric.g.montague@gmail.com>
2020-07-22 23:15:21 -0400

    Changed structure of jsx and changed class names

--- src/components/profile/Plan.js
+++ src/components/profile/Plan.js
@@ -1,25 +1,24 @@
 import React from "react";
+import { formatDateTime } from "../../services/dates";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
 
 const Plan = props => {
+  console.log(props.plan.datetime);
   return (
-    <div className="panel">
-      <img src={props.plan.image} alt="Plan image" />
-      <div className="panel-body">
-        <h3 className="panel-title">{props.plan.title}</h3>
-        <div className="panel-content">
-          <p>{props.plan.description}</p>
+    <div className="plan-card" onClick={() => console.log("Show itinerary")}>
+      <div className="plan-card-image-wrapper">
+        <img src={props.plan.image} alt="Plan" />
+      </div>
+      <div className="plan-card-body">
+        <h3 className="plan-card-title">{props.plan.title}</h3>
+        <div className="flex align-items space-between">
+          <p className="text-medium font-size-sm">
+            {formatDateTime(props.plan.datetime)}
+          </p>
         </div>
-        <div className="panel-footer space-between">
-          <div className="flex align-items space-between">
-            <p>Start datetime: {props.plan.datetime}</p>
-            <p>{props.plan.placeIds.length} Places</p>
-          </div>
-          <div className="flex align-items space-between">
-            <FontAwesomeIcon icon={["fa", "edit"]} />
-            <FontAwesomeIcon icon={["fa", "trash-alt"]} />
-          </div>
+        <div className="plan-card-content py-1">
+          <p>{props.plan.description}</p>
         </div>
       </div>
       <div className="icon-container">
@@ -40,7 +39,7 @@ Plan.propTypes = {
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
-    datetime: PropTypes.string.isRequired,
+    datetime: PropTypes.instanceOf(Date).isRequired,
     image: PropTypes.string.isRequired,
     placeIds: PropTypes.arrayOf(PropTypes.string.isRequired)
   }),
