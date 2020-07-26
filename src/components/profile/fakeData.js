557e9571782ee604473a3fc2110bf38e7650a1cc
Eric Montague <eric.g.montague@gmail.com>
2020-07-26 19:24:02 -0400

    created a module to hold fake data for the application

---
+++ src/components/profile/fakeData.js
@@ -0,0 +1,32 @@
+import { v4 as uuidv4 } from "uuid";
+import cityImage from "../../assets/city_life.jpg";
+
+export const fakePlans = [
+  {
+    id: uuidv4(),
+    title: "Plan Title One",
+    description:
+      "Try reading numbers from left to right. As you go left to right, you insert values in decreasing magnitude: year, month, day, hours, minutes.",
+    datetime: new Date(),
+    image: cityImage,
+    placeIds: [uuidv4(), uuidv4(), uuidv4()]
+  },
+  {
+    id: uuidv4(),
+    title: "Plan Title Two",
+    description:
+      "Try reading numbers from left to right. As you go left to right, you insert values in decreasing magnitude: year, month, day, hours, minutes.",
+    datetime: new Date(),
+    image: cityImage,
+    placeIds: [uuidv4(), uuidv4(), uuidv4()]
+  },
+  {
+    id: uuidv4(),
+    title: "Plan Title Three",
+    description:
+      "Try reading numbers from left to right. As you go left to right, you insert values in decreasing magnitude: year, month, day, hours, minutes.",
+    datetime: new Date(),
+    image: cityImage,
+    placeIds: [uuidv4(), uuidv4(), uuidv4()]
+  }
+];
