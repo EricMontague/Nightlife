8f482ad69e743d37e6a37c4ede25308c26983ef8
Eric Montague <eric.g.montague@gmail.com>
2020-07-26 23:12:38 -0400

    added new route from plans

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import DeviceProvider from "./context/DeviceProvider";
 import AuthApp from "./components/authentication/AuthApp";
 import Home from "./components/authentication/Home";
 import ProfileApp from "./components/profile/ProfileApp";
+import PlanApp from "./components/plans/PlanApp";
 import NotFound from "./components/common/NotFound";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -27,6 +28,13 @@ const App = () => {
               exact
               path="/users/:displayName"
               render={() => <ProfileApp />}
+            />
+            <Route
+              exact
+              path="/plans"
+              render={({ match, history }) => (
+                <PlanApp match={match} history={history} />
+              )}
             />
             <Route exact path="*" render={() => <NotFound />} />
           </Switch>
