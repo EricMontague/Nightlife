89a7853f5a1b360f43022fa87cde5295c059e201
Eric Montague <eric.g.montague@gmail.com>
2020-07-27 23:46:41 -0400

    changed name of class from flex to flex-row

---
+++ src/components/plans/PlaceDetailsModal.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const PlaceDetailsModal = props => {
+  return <h1>Place details modal rendered</h1>;
+};
+
+export default PlaceDetailsModal;
