8e44cde2eddefac1042aa8fdcf0540d1bae5e654
Eric Montague <eric.g.montague@gmail.com>
2020-07-28 23:44:08 -0400

    created datepicker component

---
+++ src/components/common/DatePicker.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const DatePicker = props => {
+  return (
+    <div className="input-group">
+      <input
+        type="date"
+        autoFocus={props.autoFocus}
+        name={props.name}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.handleChange}
+        className={props.error ? "input-with-error" : ""}
+        aria-label={props.name}
+      />
+    </div>
+  );
+};
+
+DatePicker.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  error: PropTypes.string.isRequired
+};
+
+export default DatePicker;
