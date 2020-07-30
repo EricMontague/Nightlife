7831f8f75313386c3aaa893cae713e09eb36a893
Eric Montague <eric.g.montague@gmail.com>
2020-07-29 23:49:25 -0400

    created date format validator

--- src/services/validators.js
+++ src/services/validators.js
@@ -20,17 +20,18 @@ export const validateLength = (min, max) => {
 // very simple email regex. Not meant to be exhaustive
 export const validateEmail = value => {
   let message = "";
-  const regex = /^([a-zA-z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/;
-  if (!regex.test(value)) {
+  const emailRegexp = /^([a-zA-z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/;
+  if (!emailRegexp.test(value)) {
     message = "Please provide a valid email address";
   }
   return message;
 };
 
-export const validateDate = (min, max) => {
+// expects input to be string
+export const validateDateRange = (min, max) => {
   const validate = dateInput => {
     let message = "";
-    const date = new Date(date);
+    const date = new Date(dateInput);
     const minDate = new Date(min);
     const maxDate = new Date(max);
     if (min && max) {
@@ -48,5 +49,18 @@ export const validateDate = (min, max) => {
     }
     return message;
   };
+  return validate;
+};
+
+// expects input to be string
+export const validateDateFormat = (dateRegexp, format) => {
+  const validate = dateInput => {
+    let message = "";
+    if (!dateRegexp.test(dateInput)) {
+      message = `Date format should be ${format}`;
+    }
+    return message;
+  };
+
   return validate;
 };
