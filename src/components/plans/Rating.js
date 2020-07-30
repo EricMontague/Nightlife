49757bd54af966fcb7e10d7c147254ac8ab2e4dd
Eric Montague <eric.g.montague@gmail.com>
2020-07-29 23:48:35 -0400

    created Rating compoennt

---
+++ src/components/plans/Rating.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from "prop-types";
+
+const Rating = props => {
+  const renderStars = () => {
+    const starList = [];
+    for (let star = 0; star < parseInt(props.stars); star++) {
+      starList.push(<FontAwesomeIcon key={star} icon={["fa", "star"]} />);
+    }
+    // handle ratings that are floats
+    if (props.stars % 1 !== 0) {
+      starList.push(
+        <FontAwesomeIcon key={starList.length} icon={["fa", "star-half"]} />
+      );
+    }
+    return starList;
+  };
+
+  return <div className="rating">{renderStars()}</div>;
+};
+
+Rating.propTypes = {
+  stars: PropTypes.number.isRequired
+};
+
+export default Rating;
