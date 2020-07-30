928399d15ddb21fdb794c839450fd05ef4705574
Eric Montague <eric.g.montague@gmail.com>
2020-07-29 23:46:56 -0400

    no changes

--- src/components/common/DatePicker.js
+++ src/components/common/DatePicker.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const DatePicker = props => {
+  console.log(props.value);
   return (
     <div className="input-group">
       <input
