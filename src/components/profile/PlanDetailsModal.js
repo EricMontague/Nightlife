6d854e0987e429d61b9f605c390cd2c24a978bf8
Eric Montague <eric.g.montague@gmail.com>
2020-08-01 00:29:38 -0400

    changed the name of an imported file

--- src/components/profile/PlanDetailsModal.js
+++ src/components/profile/PlanDetailsModal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { formatDateTime } from "../../services/dates";
+import { formatDateTime } from "../../services/dateTimeHelpers";
 import PropTypes from "prop-types";
 
 const PlanDetailsModal = props => {
