563569c333d74648b9f6f66e365ff2f2aed2b451
Eric Montague <eric.g.montague@gmail.com>
2020-08-09 22:44:40 -0400

    Moved the jsx forthe HomeContent component into the Home component

--- src/components/authentication/Home.js
+++ src/components/authentication/Home.js
@@ -1,21 +1,42 @@
 import React from "react";
-import Card from "../common/Card";
-import CenteredPageLayout from "../common/CenteredPageLayout";
-import HomeContent from "./HomeContent";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
 
 const Home = props => {
   return (
-    <CenteredPageLayout>
-      <Card classes="card-medium">
-        <HomeContent signInWithGoogleOAuth={props.signInWithGoogleOAuth} />
-      </Card>
-    </CenteredPageLayout>
+    <>
+      <div className="card-header text-center">
+        <FontAwesomeIcon
+          icon={["fas", "glass-cheers"]}
+          size="3x"
+          className="logo-primary mb-1"
+        />
+        <h3 className="card-title">Welcome to Nightlife</h3>
+        <p className="lead">Plan your night out</p>
+      </div>
+
+      <Link to="/signup" className="btn btn-primary btn-block mb-1 py-2">
+        Continue with email
+      </Link>
+      <button
+        type="button"
+        onClick={props.signInWithGoogle}
+        className="btn btn-secondary btn-block mb-1 py-2"
+      >
+        Continue with Google
+      </button>
+      <div className="card-footer flex justify-center">
+        <p>
+          Already a member? <Link to="/signin"> Sign In</Link>
+        </p>
+      </div>
+    </>
   );
 };
 
 Home.propTypes = {
-  signInWithGoogleOAuth: PropTypes.func.isRequired
+  signInWithGoogle: PropTypes.func.isRequired
 };
 
 export default Home;
