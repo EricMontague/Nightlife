06f12093665483989f6062cf63b916311bf57c61
Eric Montague <eric.g.montague@gmail.com>
2020-08-09 22:45:56 -0400

    Added an if statement to handle the address line when a place doesn't have a priceLevel

--- src/components/plans/Place.js
+++ src/components/plans/Place.js
@@ -4,14 +4,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
 
 const Place = props => {
+  let addressLine =
+    "$".repeat(props.place.priceLevel) + " | " + props.place.formattedAddress;
+  if (!props.place.priceLevel) {
+    addressLine = props.place.formattedAddress;
+  }
   return (
     <div className="place-card">
       <div className="place-card-body">
         <h4 className="place-card-title">{props.place.name}</h4>
         <Rating stars={props.place.rating} />
-        <p>
-          {"$".repeat(props.place.priceLevel)} | {props.place.formattedAddress}
-        </p>
+        <p>{addressLine}</p>
       </div>
       <div className="place-card-footer">
         <p>View Details</p>
