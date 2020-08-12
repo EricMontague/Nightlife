d8d58a64dc9c7fc1a8ce81fedeec80aa9b235686
Eric Montague <eric.g.montague@gmail.com>
2020-08-12 00:19:03 -0400

    Deleted axios file

--- src/services/axiosGoogleMaps.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import constants from "./constants";
-
-export const axiosPlaceDetailsInstance = axios.create({
-  baseURL: constants.PLACE_DETAILS_URL,
-  timeout: 10000
-});
