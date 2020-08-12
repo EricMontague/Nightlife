dfd60d43d494f8900d470f30349cbdbd43b3e1fa
Eric Montague <eric.g.montague@gmail.com>
2020-08-12 00:17:06 -0400

    Finished implementation for fetching the plans from firebase and then fetching a photo from the Places api to go along with each plan

--- src/components/profile/ProfileApp.js
+++ src/components/profile/ProfileApp.js
@@ -6,7 +6,6 @@ import ProfileHeader from "./ProfileHeader";
 import ProfileContent from "./ProfileContent";
 import PlanDetailsModal from "./PlanDetailsModal";
 import DeletePlanModal from "./DeletePlanModal";
-import { axiosPlaceDetailsInstance } from "../../services/axiosGoogleMaps";
 
 class ProfileApp extends React.Component {
   constructor() {
@@ -17,29 +16,68 @@ class ProfileApp extends React.Component {
       selectedPlan: null,
       plans: []
     };
+
+    this.hasPlacesLibraryScript = this.hasPlacesLibraryScript.bind(this);
+    this.loadScriptUrl = this.loadScriptUrl.bind(this);
+    this.fetchData = this.fetchData.bind(this);
     this.fetchPlans = this.fetchPlans.bind(this);
     this.fetchPhotoUrls = this.fetchPhotoUrls.bind(this);
     this.deletePlan = this.deletePlan.bind(this);
     this.togglePlanDetailsModal = this.togglePlanDetailsModal.bind(this);
     this.toggleDeletePlanModal = this.toggleDeletePlanModal.bind(this);
+    this.googleMapsScriptUrl = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GCP_API_KEY}`;
+    window.fetchData = this.fetchData; // necessary to properly setup the callback for the places API
   }
 
   static contextType = AuthContext;
 
-  // componentDidMount() {
-  //   this.fetchPlans()
-  //     .then(plans => {
-  //       return this.fetchPhotoUrls(plans);
-  //     })
-  //     .then(updatedPlans => {
-  //       this.setState({ plans: updatedPlans });
-  //     })
-  //     .catch(error => {
-  //       console.log(
-  //         `An error occurred while retrieving plans and photos: ${error.message}`
-  //       );
-  //     });
-  // }
+  componentDidMount() {
+    const placesLibrary = "&libraries=places";
+    const callback = "&callback=fetchData";
+    if (!this.hasPlacesLibraryScript(placesLibrary)) {
+      this.loadScriptUrl(this.googleMapsScriptUrl + callback + placesLibrary);
+    }
+  }
+
+  componentWillUnmount() {
+    // Remove google maps script if present
+    const placesLibrary = "libraries=places";
+    if (this.hasPlacesLibraryScript(placesLibrary)) {
+      document
+        .querySelector(`script[src^="${this.googleMapsScriptUrl}"]`)
+        .remove();
+    }
+  }
+
+  hasPlacesLibraryScript(libraryParameters) {
+    let hasPlacesLibrary = false;
+    document
+      .querySelectorAll(`script[src^="${this.googleMapsScriptUrl}"]`)
+      .forEach(scriptTag => {
+        if (scriptTag.src.includes(libraryParameters)) {
+          hasPlacesLibrary = true;
+        }
+      });
+    return hasPlacesLibrary;
+  }
+
+  loadScriptUrl(scriptUrl) {
+    const scriptElement = document.createElement("script");
+    scriptElement.src = scriptUrl;
+    scriptElement.type = "text/javascript";
+    document.body.appendChild(scriptElement);
+  }
+
+  async fetchData() {
+    try {
+      const plans = await this.fetchPlans();
+      this.fetchPhotoUrls(plans);
+    } catch (error) {
+      console.log(
+        `An error occurred while retrieving plans and photos: ${error.message}`
+      );
+    }
+  }
 
   async fetchPlans() {
     try {
@@ -49,13 +87,30 @@ class ProfileApp extends React.Component {
     }
   }
 
-  async fetchPhotoUrls(plans) {
-    return plans.map(async plan => {
-      const placeResults = await axiosPlaceDetailsInstance.get("/", {
-        params: { fields: "photo", place_id: plan.placeIds[0] }
-      });
-      plan.photoUrl = placeResults.photos[0].getURL();
-      return plan;
+  fetchPhotoUrls(plans) {
+    const placesService = new window.google.maps.places.PlacesService(
+      document.createElement("div")
+    );
+    plans.forEach(plan => {
+      placesService.getDetails(
+        {
+          fields: ["photo"],
+          placeId: plan.placeIds[0]
+        },
+        (placeResults, status) => {
+          if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+            const updatedPlan = {
+              ...plan,
+              image: placeResults.photos[0].getUrl()
+            };
+            this.setState({ plans: [...this.state.plans, updatedPlan] });
+          } else {
+            console.log(
+              `There was an error retrieving the place's photo: ${status}`
+            );
+          }
+        }
+      );
     });
   }
 
