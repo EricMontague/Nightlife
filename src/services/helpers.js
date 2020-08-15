366a63d8bc4b0683bded1ed47210c754a433540d
Eric Montague <eric.g.montague@gmail.com>
2020-08-14 23:42:23 -0400

    Created helper functions to assist with detection, addition, and removal of the google api scripts

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -5,3 +5,40 @@ export const trimObjectFieldValues = obj => {
   }
   return trimmedObject;
 };
+
+export const hasGoogleScript = (sourceAttributeValue, urlParameters) => {
+  let hasScript = false;
+  const scriptNodes = document.querySelectorAll(
+    `script[src^="${sourceAttributeValue}"]`
+  );
+  scriptNodes.forEach(scriptNode => {
+    urlParameters.forEach(parameter => {
+      if (scriptNode.src.includes(parameter)) {
+        hasScript = true;
+      }
+    });
+  });
+  return hasScript;
+};
+
+export const removeGoogleScript = (sourceAttributeValue, urlParameters) => {
+  let numParameters = urlParameters.length;
+  const scriptNode = document.querySelector(
+    `script[src^="${sourceAttributeValue}"]`
+  );
+  for (let index = 0; index < numParameters; index++) {
+    if (scriptNode.src.includes(urlParameters[index])) {
+      numParameters -= 1;
+    }
+  }
+  if (numParameters === 0) {
+    scriptNode.remove();
+  }
+};
+
+export const loadGoogleScript = (sourceAttributeValue, urlParameters) => {
+  const newScriptElement = document.createElement("script");
+  newScriptElement.src = sourceAttributeValue + "&" + urlParameters.join("&");
+  newScriptElement.type = "text/javascript";
+  document.body.appendChild(newScriptElement);
+};
