ff4018e107859de6f792c789f0129db9ab48c842
Eric Montague <eric.g.montague@gmail.com>
2020-08-22 00:07:29 -0400

    Added new props to allow for the dropdown menu to close if one of the links inside of it is clicked

--- src/components/common/NavLinks.js
+++ src/components/common/NavLinks.js
@@ -6,11 +6,20 @@ import { signOutUser } from "../../services/firebase";
 import PropTypes from "prop-types";
 
 const NavLinks = props => {
+  const handleLinkClick = () => {
+    if (props.isDropdownOpen) {
+      props.handleClick();
+    }
+  };
+
   if (props.isLoggedIn) {
     return (
       <>
         <li>
-          <Link to={`/users/${props.currentUser.displayName.replace(" ", "")}`}>
+          <Link
+            to={`/users/${props.currentUser.displayName.replace(" ", "")}`}
+            onClick={handleLinkClick}
+          >
             <UserInfo
               user={props.currentUser}
               avatarClasses="img-rounded avatar-sm"
@@ -18,7 +27,7 @@ const NavLinks = props => {
           </Link>
         </li>
         <li>
-          <Link to="/plans/create">
+          <Link to="/plans/create" onClick={handleLinkClick}>
             <FontAwesomeIcon icon={["fas", "plus-circle"]} />
             Create List
           </Link>
@@ -28,6 +37,7 @@ const NavLinks = props => {
             href="/logout"
             onClick={event => {
               event.preventDefault();
+              handleLinkClick();
               signOutUser()
                 .then(() => {
                   console.log("Sign out successful!");
@@ -44,10 +54,14 @@ const NavLinks = props => {
     return (
       <>
         <li>
-          <Link to="/signin">Sign In</Link>
+          <Link to="/signin" onClick={handleLinkClick}>
+            Sign In
+          </Link>
         </li>
         <li>
-          <Link to="/signup">Sign Up</Link>
+          <Link to="/signup" onClick={handleLinkClick}>
+            Sign Up
+          </Link>
         </li>
       </>
     );
@@ -56,7 +70,9 @@ const NavLinks = props => {
 
 NavLinks.propTypes = {
   currentUser: PropTypes.objectOf(PropTypes.string.isRequired),
-  isLoggedIn: PropTypes.bool.isRequired
+  isLoggedIn: PropTypes.bool.isRequired,
+  handleClick: PropTypes.func,
+  isDropdownOpen: PropTypes.bool.isRequired
 };
 
 export default NavLinks;
