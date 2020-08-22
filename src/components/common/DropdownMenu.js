3ce5c36fba3724d4610c8f6f7d8c7b8a9aa28e7b
Eric Montague <eric.g.montague@gmail.com>
2020-08-22 00:11:25 -0400

    Added ability to close the modal by clicking outside of it and changed the lines of codef or enabling and disabling pointer events into funcitons

--- src/components/common/DropdownMenu.js
+++ src/components/common/DropdownMenu.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
+import { disableScrollY, enableScrollY } from "../../services/helpers";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 import NavLinks from "./NavLinks";
 import PropTypes from "prop-types";
 
 const DropdownMenu = props => {
   if (props.isOpen) {
-    document.body.classList.add("no-scroll-y");
+    disableScrollY();
   } else {
-    document.body.classList.remove("no-scroll-y");
+    enableScrollY();
   }
+
+  const menuRef = useRef();
+
+  // Necessary for when a user clicks a link to another page
+  const closeMenu = () => {
+    menuRef.current.parentElement.classList.replace("show", "hide");
+    menuRef.current.classList.remove("slide-in");
+    props.toggleMenu(false);
+  };
+
+  useOnClickOutside(menuRef, closeMenu);
+
   return (
     <div className={"dropdown-overlay" + (props.isOpen ? " show" : " hide")}>
       <ul
         className={
           "dropdown-menu bg-dark h-100vh" + (props.isOpen ? " slide-in" : "")
         }
+        ref={menuRef}
       >
         <div
           className="close"
@@ -26,6 +41,8 @@ const DropdownMenu = props => {
         <NavLinks
           currentUser={props.currentUser}
           isLoggedIn={props.isLoggedIn}
+          handleClick={closeMenu}
+          isDropdownOpen={props.isOpen}
         />
       </ul>
     </div>
