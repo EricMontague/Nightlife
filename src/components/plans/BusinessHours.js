346620953898307d023d53113cf075fa1f4511b8
Eric Montague <eric.g.montague@gmail.com>
2020-08-22 23:46:57 -0400

    Make it optional to pass in business hours

--- src/components/plans/BusinessHours.js
+++ src/components/plans/BusinessHours.js
@@ -2,16 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const BusinessHours = props => {
-  return (
-    <div className="place-business-hours">
-      <h3>Business Hours:</h3>
-      <ul>
-        {props.businessHours.map((hours, index) => {
-          return <li key={index}>{hours}</li>;
-        })}
-      </ul>
-    </div>
-  );
+  if (props.businessHours.length > 0) {
+    return (
+      <div className="place-business-hours">
+        <h3>Business Hours:</h3>
+        <ul>
+          {props.businessHours.map((hours, index) => {
+            return <li key={index}>{hours}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 BusinessHours.propTypes = {
