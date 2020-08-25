4a3654b9a6ee0f11a034bf9e79f5f5cd40f338de
Eric Montague <eric.g.montague@gmail.com>
2020-08-24 23:38:09 -0400

    Added new helper methods for reOrder places

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -43,26 +43,51 @@ export const loadGoogleScript = (sourceAttributeValue, urlParameters) => {
   document.body.appendChild(newScriptElement);
 };
 
+const shiftPlaceForwards = (places, sourceIndex, destinationIndex) => {
+  const placesCopy = places.slice();
+  const target = placesCopy[sourceIndex];
+  while (sourceIndex < destinationIndex) {
+    placesCopy[sourceIndex] = placesCopy[sourceIndex + 1];
+    placesCopy[sourceIndex].sortKey -= 1;
+    sourceIndex += 1;
+  }
+  if (sourceIndex === destinationIndex) {
+    placesCopy[sourceIndex] = target;
+    target.sortKey = sourceIndex;
+  }
+  return placesCopy;
+};
+
+const shiftPlaceBackwards = (places, sourceIndex, destinationIndex) => {
+  const placesCopy = places.slice();
+  const target = placesCopy[sourceIndex];
+  while (sourceIndex > destinationIndex) {
+    placesCopy[sourceIndex] = placesCopy[sourceIndex - 1];
+    placesCopy[sourceIndex].sortKey += 1;
+    sourceIndex -= 1;
+  }
+  if (sourceIndex === destinationIndex) {
+    placesCopy[sourceIndex] = target;
+    target.sortKey = sourceIndex;
+  }
+  return placesCopy;
+};
+
 export const reorderPlaces = (places, sourceIndex, destinationIndex) => {
   const length = places.length;
   if (
     sourceIndex < 0 ||
     sourceIndex >= length ||
     destinationIndex < 0 ||
-    destinationIndex >= length
+    destinationIndex >= length ||
+    sourceIndex === destinationIndex
   ) {
-    throw new Error("Array index out of bounds.");
+    return places;
   }
-  const placesCopy = places.slice();
-  const target = placesCopy[sourceIndex];
-  while (destinationIndex < sourceIndex) {
-    placesCopy[sourceIndex] = placesCopy[sourceIndex - 1];
-    sourceIndex -= 1;
+  if (sourceIndex < destinationIndex) {
+    return shiftPlaceForwards(places, sourceIndex, destinationIndex);
   }
-  if (sourceIndex === destinationIndex) {
-    placesCopy[sourceIndex] = target;
-  }
-  return placesCopy;
+  return shiftPlaceBackwards(places, sourceIndex, destinationIndex);
 };
 
 export const disablePointerEvents = () => {
