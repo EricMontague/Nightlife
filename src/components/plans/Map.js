eba12efc5b043b9bee815e8825528c109f4d97d5
Eric Montague <eric.g.montague@gmail.com>
2020-08-25 22:47:53 -0400

    Added methods to handle when a place's marker is hovered over

--- src/components/plans/Map.js
+++ src/components/plans/Map.js
@@ -2,17 +2,51 @@ import React from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 export class MapContainer extends React.Component {
+  onMouseout(placedId) {
+    const innerMouseout = (props, marker, event) => {
+      this.props.handleMouseout();
+    };
+    return innerMouseout;
+  }
+
+  onMouseover(placeId) {
+    const innerMouseover = (props, marker, event) => {
+      this.props.handleMouseover(placeId);
+    };
+    return innerMouseover;
+  }
+
   render() {
     return (
       <Map
         google={this.props.google}
         center={{
-          lat: 40.854885,
-          lng: -88.081807
+          lat: this.props.places[0].location.lat(),
+          lng: this.props.places[0].location.lng()
         }}
         zoom={8}
       >
-        <Marker name={"Current location"} />
+        <Marker
+          name={this.props.places[0].name}
+          position={{
+            lat: this.props.places[0].location.lat(),
+            lng: this.props.places[0].location.lng()
+          }}
+          onClick={(props, marker, e) => console.log("Hi")}
+          onMouseover={(props, marker, e) => console.log("Mouseover")}
+        />
+        {/* {this.props.places.map(place => {
+          return (
+            <Marker
+              key={place.placeId}
+              name={place.name}
+              position={{
+                lat: place.location.lat(),
+                lng: place.location.lng()
+              }}
+            />
+          );
+        })} */}
 
         <InfoWindow>
           <div>
