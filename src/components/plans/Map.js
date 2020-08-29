3f34f7e08741926e04eed9576c1514b5b511dd8a
Eric Montague <eric.g.montague@gmail.com>
2020-08-29 00:19:53 -0400

    Figured out how to dynamically add and remove markers and well as have info windows pop up when hovering over a specfic marker

--- src/components/plans/Map.js
+++ src/components/plans/Map.js
@@ -1,42 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CustomMarker from "./CustomMarker";
-import MarkerGroup from "./MarkerGroup";
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import { Map, Marker, InfoWindow, GoogleApiWrapper } from "google-maps-react";
 
-export class MapContainer extends React.PureComponent {
+export class MapContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedPlaceId: "",
+      selectedPlace: null,
       activeMarker: null
     };
-    this.onMouseoutMarker = this.onMouseoutMarker.bind(this);
     this.onMouseoverMarker = this.onMouseoverMarker.bind(this);
-    this.setSelectedPlaceId = this.setSelectedPlaceId.bind(this);
+    this.setSelectedPlace = this.setSelectedPlace.bind(this);
     this.setActiveMarker = this.setActiveMarker.bind(this);
     this.handleWindowOpen = this.handleWindowOpen.bind(this);
     this.handleWindowClose = this.handleWindowClose.bind(this);
     this.handleLinkClick = this.handleLinkClick.bind(this);
   }
 
-  onMouseoutMarker(props, marker, event) {
-    this.props.handleMouseout();
-    this.setSelectedPlaceId("");
+  shouldComponentUpdate(nextProps, nextState) {
+    // Until an infowindow is opened, this component should update
+    if (this.state.selectedPlace == null && this.state.activeMarker === null) {
+      return true;
+    }
+    // The component should only update if a user moves their cursor over a different
+    // marker
+    return (
+      nextState.selectedPlace !== this.state.selectedPlace ||
+      nextState.activeMarker !== this.state.activeMarker
+    );
   }
 
-  onMouseoverMarker(placeId) {
+  onMouseoverMarker(place) {
     const innerHandler = (props, marker, event) => {
-      this.props.handleMouseover(placeId);
-      this.setSelectedPlaceId(placeId);
+      this.props.handleMouseover(place.placeId);
+      this.setSelectedPlace(place);
       this.setActiveMarker(marker);
     };
     return innerHandler;
   }
 
-  setSelectedPlaceId(placeId) {
+  setSelectedPlace(place) {
     this.setState({
-      selectedPlaceId: placeId
+      selectedPlace: place
     });
   }
 
@@ -46,28 +51,24 @@ export class MapContainer extends React.PureComponent {
     });
   }
 
-  handleWindowOpen(props, event) {
+  handleWindowOpen(place, event) {
     const windowContent = (
       <React.Fragment>
-        <h3 className="text-overflow-ellipsis mb-2">{props.places[0].name}</h3>
-        <span
-          className="link"
-          onClick={this.handleLinkClick(this.props.places[0])}
-        >
+        <h3 className="text-overflow-ellipsis mb-2">{place.name}</h3>
+        <span className="link" onClick={this.handleLinkClick(place)}>
           View Details
         </span>
       </React.Fragment>
     );
     ReactDOM.render(
       React.Children.only(windowContent),
-      document.getElementById(props.places[0].placeId + props.places[0].name)
+      document.getElementById(place.placeId + place.name)
     );
   }
 
   handleWindowClose() {
-    this.setState({
-      activeMarker: null
-    });
+    this.setSelectedPlace(null);
+    this.setActiveMarker(null);
   }
 
   handleLinkClick(place) {
@@ -79,7 +80,11 @@ export class MapContainer extends React.PureComponent {
   }
 
   render() {
-    // const { place } = this.props.places[0];
+    let infoWindowId = "";
+    if (this.state.selectedPlace !== null) {
+      infoWindowId =
+        this.state.selectedPlace.placeId + this.state.selectedPlace.name;
+    }
 
     return (
       <Map
@@ -90,7 +95,47 @@ export class MapContainer extends React.PureComponent {
         }}
         zoom={10}
       >
-        <CustomMarker
+        {this.props.shouldRenderMarkers &&
+          this.props.places.map(place => {
+            const { placeId, name, location } = place;
+            return (
+              <Marker
+                key={placeId}
+                title={name}
+                name={name}
+                position={{
+                  lat: location.lat(),
+                  lng: location.lng()
+                }}
+                onMouseover={this.onMouseoverMarker(place)}
+                onMouseout={this.props.handleMouseout}
+                map={this.props.map}
+                google={this.props.google}
+                mapCenter={this.props.mapCenter}
+              />
+            );
+          })}
+
+        <InfoWindow
+          visible={this.state.activeMarker === null ? false : true}
+          marker={this.state.activeMarker}
+          onOpen={event => {
+            this.handleWindowOpen(this.state.selectedPlace, event);
+          }}
+          onClose={this.handleWindowClose}
+        >
+          <div id={infoWindowId}></div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_GCP_API_KEY
+})(MapContainer);
+
+/* <CustomMarker
           key={this.props.places[0].placeId}
           title={this.props.places[0].name}
           name={this.props.places[0].name}
@@ -113,53 +158,20 @@ export class MapContainer extends React.PureComponent {
           <div
             id={this.props.places[0].placeId + this.props.places[0].name}
           ></div>
-        </InfoWindow>
+        </InfoWindow> */
 
-        {/* {this.props.places.map(place => {
-          return (
-            <MarkerGroup
-              key={place.placeId}
-              place={place}
-              handleMouseover={this.onMouseoverMarker(place.placeId)}
-              handleMouseout={this.onMouseoutMarker}
-              toggleModal={this.props.toggleModal}
-              activeMarker={this.state.activeMarker}
-              map={this.props.map}
-              google={this.props.google}
-              mapCenter={this.props.mapCenter}
-            />
-          );
-        })} */}
-      </Map>
-    );
-  }
-}
-
-export default GoogleApiWrapper({
-  apiKey: process.env.REACT_APP_GCP_API_KEY
-})(MapContainer);
-
-// <CustomMarker
-//   key={place.placeId}
-//   title={place.name}
-//   name={place.name}
-//   position={{
-//     lat: place.location.lat(),
-//     lng: place.location.lng()
-//   }}
-//   onMouseover={this.onMouseoverMarker(place.placeId)}
-//   onMouseout={this.onMouseoutMarker}
-// />
-
-// <InfoWindow
-//   visible={true}
-//   marker={this.state.activeMarker}
-//   style={styles.infoWindow}
-// >
-//   <div>
-//     <h3>{place.name}</h3>
-//     <p className="link" onClick={this.props.toggleModal}>
-//       View Details
-//     </p>
-//   </div>
-// </InfoWindow>
+// return (
+//   <MarkerGroup
+//     key={place.placeId}
+//     place={place}
+//     handleMouseover={this.onMouseoverMarker(place.placeId)}
+//     handleMouseout={this.onMouseoutMarker}
+//     handleLinkClick={this.handleLinkClick}
+//     handleWindowOpen={this.handleWindowOpen}
+//     handleWindowClose={this.handleWindowClose}
+//     activeMarker={this.state.activeMarker}
+//     map={this.props.map}
+//     google={this.props.google}
+//     mapCenter={this.props.mapCenter}
+//   />
+// );
