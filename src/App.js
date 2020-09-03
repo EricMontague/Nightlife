1f81509230313a2ede8a23e5a270ca46582ad322
Eric Montague <eric.g.montague@gmail.com>
2020-09-02 23:09:06 -0400

    Remove DeviceProvider and AlertProvider

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./components/common/Navbar";
 import AlertProvider from "./context/AlertProvider";
 import AuthProvider from "./context/AuthProvider";
-import DeviceProvider from "./context/DeviceProvider";
 import AuthApp from "./components/authentication/AuthApp";
 import ProfileApp from "./components/profile/ProfileApp";
 import PlanApp from "./components/plans/PlanApp";
@@ -11,45 +10,37 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <AlertProvider>
-      <DeviceProvider>
-        <AuthProvider>
-          <Router>
-            <Navbar />
-            <Switch>
-              <Route
-                exact
-                path={["/", "/signin", "/signup"]}
-                render={({ match, history }) => (
-                  <AuthApp match={match} history={history} />
-                )}
-              />
-              <Route
-                exact
-                path="/users/:displayName"
-                render={() => <ProfileApp />}
-              />
-              <Route
-                exact
-                path={[
-                  "/plans/create",
-                  "/plans/:plan_id/view",
-                  "/plans/:plan_id/edit"
-                ]}
-                render={({ match, history, location }) => (
-                  <PlanApp
-                    match={match}
-                    history={history}
-                    location={location}
-                  />
-                )}
-              />
-              <Route exact path="*" render={() => <NotFound />} />
-            </Switch>
-          </Router>
-        </AuthProvider>
-      </DeviceProvider>
-    </AlertProvider>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route
+            exact
+            path={["/", "/signin", "/signup"]}
+            render={({ match, history }) => (
+              <AuthApp match={match} history={history} />
+            )}
+          />
+          <Route
+            exact
+            path="/users/:displayName"
+            render={() => <ProfileApp />}
+          />
+          <Route
+            exact
+            path={[
+              "/plans/create",
+              "/plans/:plan_id/view",
+              "/plans/:plan_id/edit"
+            ]}
+            render={({ match, history, location }) => (
+              <PlanApp match={match} history={history} location={location} />
+            )}
+          />
+          <Route exact path="*" render={() => <NotFound />} />
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 };
 
