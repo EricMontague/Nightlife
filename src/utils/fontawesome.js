eeb463fd1fa8c4ae29ddd6b332f707d18f1a3be3
Eric Montague <eric.g.montague@gmail.com>
2020-09-06 01:02:17 -0400

    Git is reporting these as untracked due to the change in directory name from 'services' to 'utils'. No changes were actually made

---
+++ src/utils/fontawesome.js
@@ -0,0 +1,28 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faGlassCheers,
+  faPlusCircle,
+  faBars,
+  faSignOutAlt,
+  faTrashAlt,
+  faEdit,
+  faListUl,
+  faChevronRight,
+  faChevronLeft,
+  faStar,
+  faStarHalfAlt
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faGlassCheers,
+  faPlusCircle,
+  faBars,
+  faSignOutAlt,
+  faTrashAlt,
+  faEdit,
+  faListUl,
+  faChevronRight,
+  faChevronLeft,
+  faStar,
+  faStarHalfAlt
+);
