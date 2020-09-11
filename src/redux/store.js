1c5c5e29fbd7c998bd6ae9de48af2615aad52af6
Eric Montague <eric.g.montague@gmail.com>
2020-09-10 21:08:36 -0400

    Added redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,13 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import reducer from "./reducers/index";
+import rootReducer from "./reducers/index";
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 export default store;
