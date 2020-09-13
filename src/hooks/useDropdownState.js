163ca480b1e4cce7d28da9580eb7b1e24873d2cf
Eric Montague <eric.g.montague@gmail.com>
2020-09-13 00:15:50 -0400

    uneeded hook

--- src/hooks/useDropdownState.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-const useDropdownState = initialState => {
-  const [isOpen, toggleState] = useState(initialState);
-  return [isOpen, toggleState];
-};
-
-export default useDropdownState;
