3ff282488f2e4c3061395a351230a89e2780ad5d
Eric Montague <eric.g.montague@gmail.com>
2020-09-13 22:58:40 -0400

    Removed the commented out old class component as well as other commented out code

--- src/components/profile/ProfileApp.js
+++ src/components/profile/ProfileApp.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 import { AlertContext } from "../../providers/AlertProvider";
 import DocumentTitle from "../navigation/DocumentTitle";
@@ -15,7 +15,6 @@ import {
 } from "../../redux/actions/planList";
 import {
   hasGoogleScript,
-  removeGoogleScript,
   loadGoogleScript
 } from "../../utils/googleMapsHelpers";
 import { enableScrollY } from "../../utils/commonHelpers";
@@ -27,7 +26,6 @@ const ProfileApp = props => {
   const dispatch = useDispatch();
   const selectedPlan = useSelector(state => state.planListReducer.selectedPlan);
   const plans = useSelector(state => {
-    console.log(state);
     return state.planListReducer.plans;
   });
   const [isPlanDetailsModalVisible, togglePlanDetailsModal] = useModalState();
@@ -69,17 +67,10 @@ const ProfileApp = props => {
   };
 
   const deletePlanHandler = planId => {
-    dispatch(deleteUserPlan(planId));
+    dispatch(deleteUserPlan(props.currentUser.userId, planId));
   };
 
-  // if (isPlanDetailsModalVisible || isDeletePlanModalVisible) {
-  //   disableScrollY();
-  //   disableNavigation();
-  // } else {
-  //   enableScrollY();
-  //   enableNavigation();
-  // }
-
+  // Sort plans
   const reverse = true;
   const sortedPlans = sortByDatetime(plans, reverse);
 
@@ -113,182 +104,9 @@ const ProfileApp = props => {
   );
 };
 
+ProfileApp.propTypes = {
+  currentUser: PropTypes.objectOf(PropTypes.string.isRequired),
+  isLoggedIn: PropTypes.bool.isRequired
+};
+
 export default ProfileApp;
-
-// class ProfileApp extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       isPlanDetailsModalVisible: false,
-//       isDeletePlanModalVisible: false,
-//       selectedPlan: null,
-//       plans: []
-//     };
-
-//     this.getInitialState = this.getInitialState.bind(this);
-//     this.fetchPlans = this.fetchPlans.bind(this);
-//     this.fetchPlanPhotos = this.fetchPlanPhotos.bind(this);
-//     this.setInitialState = this.setInitialState.bind(this);
-//     this.deletePlan = this.deletePlan.bind(this);
-//     this.togglePlanDetailsModal = this.togglePlanDetailsModal.bind(this);
-//     this.toggleDeletePlanModal = this.toggleDeletePlanModal.bind(this);
-//   }
-
-//   componentDidMount() {
-//     const urlParameters = ["libraries=" + constants.GOOGLE_LIBRARIES.places];
-//     if (!hasGoogleScript(constants.GOOGLE_MAPS_SCRIPT_URL, urlParameters)) {
-//       urlParameters.push("callback=" + this.getInitialState.name.slice(6));
-//       loadGoogleScript(constants.GOOGLE_MAPS_SCRIPT_URL, urlParameters);
-//     } else {
-//       this.getInitialState();
-//     }
-//   }
-
-//   componentWillUnmount() {
-//     console.log("Profile App unmount");
-//     enableScrollY();
-//     // Commented these lines of code out to prevent a bug that was causing the
-//     // removeGoogleScript function to remove both map scripts when switching
-//     // the Create Plan page. Needs further manual testing
-//     // Remove google maps script if present
-//     // const urlParameters = ["libraries=" + constants.GOOGLE_LIBRARIES.places];
-//     // if (hasGoogleScript(constants.GOOGLE_MAPS_SCRIPT_URL, urlParameters)) {
-//     //   removeGoogleScript(constants.GOOGLE_MAPS_SCRIPT_URL, urlParameters);
-//     // }
-//   }
-
-//   async getInitialState() {
-//     try {
-//       const plans = await this.fetchPlans();
-//       this.fetchPlanPhotos(plans);
-//     } catch (error) {
-//       console.log(
-//         `An error occurred while retrieving plans and photos: ${error.message}`
-//       );
-//     }
-//   }
-
-//   async fetchPlans() {
-//     try {
-//       return await getPlans(this.context.currentUser.userId);
-//     } catch (error) {
-//       console.log(`An error occurred while retrieving plans: ${error.message}`);
-//     }
-//   }
-
-//   fetchPlanPhotos(plans) {
-//     const placesService = new window.google.maps.places.PlacesService(
-//       document.createElement("div")
-//     );
-//     plans.forEach(plan => {
-//       if (plan.places.length > 0) {
-//         const placeRequest = {
-//           fields: ["photo"],
-//           placeId: plan.places[0].placeId
-//         };
-//         placesService.getDetails(placeRequest, this.setInitialState(plan));
-//       }
-//     });
-//   }
-
-//   setInitialState(plan) {
-//     const photoOptions = {
-//       maxHeight: constants.GOOGLE_IMAGE_HEIGHT,
-//       maxWidth: constants.GOOGLE_IMAGE_WIDTH
-//     };
-//     const handlePlaceResults = (placeResults, status) => {
-//       if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-//         const photoUrl = placeResults.photos
-//           ? placeResults.photos[0].getUrl(photoOptions)
-//           : defaultPlacePhoto;
-//         const updatedPlan = {
-//           ...plan,
-//           image: photoUrl
-//         };
-//         this.setState({ plans: [...this.state.plans, updatedPlan] });
-//       } else {
-//         console.log(
-//           `There was an error retrieving the place's photo: ${status}`
-//         );
-//       }
-//     };
-//     return handlePlaceResults;
-//   }
-
-//   async deletePlan(planId) {
-//     let deleted = false;
-//     try {
-//       await deletePlan(this.context.currentUser.userId, planId); // firebase function
-//       deleted = true;
-//     } catch (error) {
-//       console.log(`Error in deleting the plan: ${error.message}`);
-//     }
-//     if (deleted) {
-//       this.setState({
-//         plans: this.state.plans.filter(plan => {
-//           return plan.planId !== planId;
-//         })
-//       });
-//     }
-//   }
-
-//   togglePlanDetailsModal(plan) {
-//     this.setState({
-//       isPlanDetailsModalVisible: !this.state.isPlanDetailsModalVisible,
-//       selectedPlan: this.state.selectedPlan ? null : plan
-//     });
-//   }
-
-//   toggleDeletePlanModal(plan) {
-//     this.setState({
-//       isDeletePlanModalVisible: !this.state.isDeletePlanModalVisible,
-//       selectedPlan: this.state.selectedPlan ? null : plan
-//     });
-//   }
-
-//   render() {
-//     if (
-//       this.state.isPlanDetailsModalVisible ||
-//       this.state.isDeletePlanModalVisible
-//     ) {
-//       disableScrollY();
-//       disableNavigation();
-//     } else {
-//       enableScrollY();
-//       enableNavigation();
-//     }
-
-//     const sortedPlans = sortByDatetime(this.state.plans, true);
-
-//     return (
-//       <DocumentTitle
-//         title={`${this.context.currentUser.displayName} | Nightlife`}
-//       >
-//         <div>
-//           <ProfileHeader
-//             isLoggedIn={this.context.isLoggedIn}
-//             currentUser={this.context.currentUser}
-//           />
-//           <ProfileContent
-//             plans={sortedPlans}
-//             toggleDeletePlanModal={plan => this.toggleDeletePlanModal(plan)}
-//             togglePlanDetailsModal={plan => this.togglePlanDetailsModal(plan)}
-//           />
-//           {this.state.isPlanDetailsModalVisible && (
-//             <PlanDetailsModal
-//               plan={this.state.selectedPlan}
-//               toggleModal={plan => this.togglePlanDetailsModal(plan)}
-//             />
-//           )}
-//           {this.state.isDeletePlanModalVisible && (
-//             <DeletePlanModal
-//               toggleModal={plan => this.toggleDeletePlanModal(plan)}
-//               handleDeleteClick={planId => this.deletePlan(planId)}
-//               plan={this.state.selectedPlan}
-//             />
-//           )}
-//         </div>
-//       </DocumentTitle>
-//     );
-//   }
-// }
