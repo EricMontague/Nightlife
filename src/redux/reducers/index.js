ce8d91a4af965f1dd2b225513968d098ca1ad563
Eric Montague <eric.g.montague@gmail.com>
2020-09-19 22:59:24 -0400

    Imported planReducer'

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
 import placeListReducer from "./placeListReducer";
 import planListReducer from "./planListReducer";
+import planReducer from "./planReducer";
 import authReducer from "./authReducer";
 import googleMapsReducer from "./googleMapsReducer";
 
 const rootReducer = combineReducers({
   placeListReducer,
   planListReducer,
+  planReducer,
   authReducer,
   googleMapsReducer
 });
