011a143c55ca5c0b846da2938466cad1b4dd3316
Eric Montague <eric.g.montague@gmail.com>
2020-09-24 23:12:04 -0400

    Added custom hook for tracking previous props.

---
+++ src/hooks/usePrevious.js
@@ -0,0 +1,12 @@
+import { useEffect, useRef } from "react";
+
+const usePrevious = value => {
+  const ref = useRef(value);
+
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
+export default usePrevious;
