e2712a836f86ec5f53d75565635ab7b9741e2a06
Eric Montague <eric.g.montague@gmail.com>
2020-09-30 00:07:05 -0400

    Removed the authentication functions out of the redux directory and into a new file in the firebase directory

---
+++ src/firebase/authentication.js
@@ -0,0 +1,43 @@
+import { auth, provider } from "./firebaseApp";
+import { storeUserDocument } from "./users";
+
+export const registerUser = async user => {
+  // Create user account through firebase
+  const results = await auth.createUserWithEmailAndPassword(
+    user.email,
+    user.password
+  );
+  await storeUserDocument({
+    id: results.user.uid,
+    email: user.email,
+    password: user.password,
+    displayName: user.firstName + " " + user.lastName,
+    photoURL: "",
+    plans: []
+  });
+};
+
+export const signInWithEmailAndPassword = async (email, password) => {
+  return auth.signInWithEmailAndPassword(email, password);
+};
+
+export const signOut = async () => {
+  return auth.signOut();
+};
+
+export const signInWithGoogle = async () => {
+  auth.signInWithRedirect(provider).then(() => {
+    auth.getRedirectResult().then(result => {
+      console.log(result);
+      if (result.user) {
+        storeUserDocument({
+          id: result.user.uid,
+          email: result.user.email,
+          displayName: result.user.displayName,
+          photoURL: result.user.photoURL,
+          plans: []
+        });
+      }
+    });
+  });
+};
--- src/redux/actions/authentication.js
+++ src/redux/actions/authentication.js
@@ -4,8 +4,8 @@ import {
   enablePointerEvents
 } from "../../utils/commonHelpers";
 import Poller from "../../utils/polling";
-import { auth, provider } from "../../firebase/firebaseApp";
-import { storeUserDocument, getUserDocument } from "../../firebase/users";
+import { auth } from "../../firebase/firebaseApp";
+import { getUserDocument } from "../../firebase/users";
 
 // Get user from firestore
 const fetchUser = async (userId, dispatch) => {
@@ -53,72 +53,4 @@ export const authStateListener = () => dispatch => {
     }
   });
   return listener;
-};
-
-export const registerUser = user => async dispatch => {
-  try {
-    // Create user account through firebase
-    const results = await auth.createUserWithEmailAndPassword(
-      user.email,
-      user.password
-    );
-    storeUserDocument({
-      id: results.user.uid,
-      email: user.email,
-      password: user.password,
-      displayName: user.firstName + " " + user.lastName,
-      photoURL: "",
-      plans: []
-    });
-    // this.props.history.push("/");
-  } catch (error) {
-    console.log(
-      `An error occured while registering the user: ${error.message}`
-    );
-  }
-};
-
-export const signInWithEmailAndPassword = (email, password) => dispatch => {
-  try {
-    auth.signInWithEmailAndPassword(email, password);
-    // this.props.history.push("/");
-  } catch (error) {
-    console.log(`Error on login: ${error.message}`);
-  }
-};
-
-export const signOut = () => dispatch => {
-  auth
-    .signOut()
-    .then(() => {
-      console.log("Sign out successful!");
-    })
-    .catch(error => console.log(`Error on sign out: ${error}`));
-};
-
-export const signInWithGoogle = () => dispatch => {
-  auth.signInWithRedirect(provider).then(() => {
-    auth
-      .getRedirectResult()
-      .then(result => {
-        if (result.user) {
-          storeUserDocument({
-            id: result.user.uid,
-            email: result.user.email,
-            displayName: result.user.displayName,
-            photoURL: result.user.photoURL,
-            plans: []
-          })
-            .then(() => {
-              console.log("Successfully stored user document!");
-            })
-            .catch(error => {
-              console.log(`Erro when storing document: ${error.message}`);
-            });
-        }
-      })
-      .catch(error => {
-        console.log(`Error with getting redirect result: ${error.message}`);
-      });
-  });
 };
