2598ca719b7b4d77a68cf46edcfecb0697b2b08e
Eric Montague <eric.g.montague@gmail.com>
2020-09-30 00:05:05 -0400

    Wrapped the entire application in the AlertProvider so that I can display alert banners in any UI component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/navigation/Navbar";
 import AuthApp from "./components/authentication/AuthApp";
 import ProfileApp from "./components/profile/ProfileApp";
 import PlanApp from "./components/plans/PlanApp";
+import AlertProvider from "./providers/AlertProvider";
 import NotFound from "./components/errors/NotFound";
 import PrivateRoute from "./components/authentication/PrivateRoute";
 import { authStateListener } from "./redux/actions/authentication";
@@ -22,34 +23,40 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <Router>
-      <Navbar isLoggedIn={isLoggedIn} currentUser={currentUser} />
-      <Switch>
-        <Route
-          exact
-          path={["/", "/signin", "/signup"]}
-          render={({ match, history }) => (
-            <AuthApp
-              match={match}
-              history={history}
-              isLoggedIn={isLoggedIn}
-              currentUser={currentUser}
-            />
-          )}
-        />
-        <PrivateRoute exact path="/users/:displayName" component={ProfileApp} />
-        <PrivateRoute
-          exact
-          path={[
-            "/plans/create",
-            "/plans/:plan_id/view",
-            "/plans/:plan_id/edit"
-          ]}
-          component={PlanApp}
-        />
-        <Route exact path="*" render={() => <NotFound />} />
-      </Switch>
-    </Router>
+    <AlertProvider>
+      <Router>
+        <Navbar isLoggedIn={isLoggedIn} currentUser={currentUser} />
+        <Switch>
+          <Route
+            exact
+            path={["/", "/signin", "/signup"]}
+            render={({ match, history }) => (
+              <AuthApp
+                match={match}
+                history={history}
+                isLoggedIn={isLoggedIn}
+                currentUser={currentUser}
+              />
+            )}
+          />
+          <PrivateRoute
+            exact
+            path="/users/:displayName"
+            component={ProfileApp}
+          />
+          <PrivateRoute
+            exact
+            path={[
+              "/plans/create",
+              "/plans/:plan_id/view",
+              "/plans/:plan_id/edit"
+            ]}
+            component={PlanApp}
+          />
+          <Route exact path="*" render={() => <NotFound />} />
+        </Switch>
+      </Router>
+    </AlertProvider>
   );
 };
 
