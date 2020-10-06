50ff0911321c754c04cf57e82afb0e5f93cfa69a
Eric Montague <eric.g.montague@gmail.com>
2020-10-06 00:15:22 -0400

    Readded the getUserDocument function to the storeUserDocument function. Removing it was causing a bug where a user's document was getting overwritten every time they logged in

--- src/firebase/users.js
+++ src/firebase/users.js
@@ -20,17 +20,26 @@ export const storeUserDocument = async user => {
     throw new Error("Please provide a user");
   }
 
+  let userDocument;
+  // See if user document exists already.
+  // The getUserDocument functino will throw an error
+  // If the user doesn't exist
   try {
-    await firestore.doc(`users/${user.id}`).set({ ...user });
+    userDocument = await getUserDocument(user.id);
   } catch (error) {
-    throw new Error(error.message);
+    if (error.message === "User not found") {
+      // pass
+    } else {
+      throw new Error(error.message);
+    }
   }
-};
 
-export const deleteUser = async userId => {
-  try {
-    await firestore.doc(`users/${userId}`).delete();
-  } catch (error) {
-    throw new Error(error.message);
+  // Create new document
+  if (!userDocument) {
+    try {
+      await firestore.doc(`users/${user.id}`).set({ ...user });
+    } catch (error) {
+      throw new Error(error.message);
+    }
   }
 };
