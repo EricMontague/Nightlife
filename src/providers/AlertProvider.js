4a0097bbdd5a66f9d32e5228c1fbfc3c7ce10a93
Eric Montague <eric.g.montague@gmail.com>
2020-10-06 00:13:03 -0400

    Refactored the AlertProvider to not be a react context, but instead to connect to the redux store for updates on the alert state, and display alerts when necessary

--- src/providers/AlertProvider.js
+++ src/providers/AlertProvider.js
@@ -1,43 +1,43 @@
-import React, { useState, useRef, useContext } from "react";
+import React, { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { CLEAR_ALERT } from "../redux/actions/types";
 import PropTypes from "prop-types";
 
 const ALERT_DISPLAY_TIME = 5000;
 
-const AlertContext = React.createContext();
-
-export const useAlertContext = () => {
-  return useContext(AlertContext);
-};
-
 const AlertProvider = props => {
   // Declare hooks
   const alertRef = useRef();
-  const [alertState, setState] = useState({
-    message: "",
-    alertClassName: ""
-  });
+  const dispatch = useDispatch();
+  const alert = useSelector(state => state.alertReducer);
+  let timeoutId;
 
-  const show = () => {
-    if (alertState.message) {
+  const showAlert = () => {
+    if (alert.message) {
       alertRef.current.classList.add("show");
-      setTimeout(close, ALERT_DISPLAY_TIME);
+      timeoutId = setTimeout(close, ALERT_DISPLAY_TIME);
     }
   };
 
   const close = () => {
     alertRef.current.classList.remove("show");
-    setState({ message: "", alertClassName: "" });
+    dispatch({
+      type: CLEAR_ALERT,
+      message: "",
+      alertClassName: ""
+    });
+    clearTimeout(timeoutId);
+    timeoutId = null;
   };
 
-  const setAlertState = newState => {
-    setState(newState);
-  };
+  // Shows an alert if there's a message in the redux store
+  showAlert();
 
   return (
-    <AlertContext.Provider value={{ showAlert: show, setAlertState }}>
+    <div>
       <div className="alert" ref={alertRef}>
-        <div className={"alert-content " + alertState.alertClassName}>
-          <p>{alertState.message}</p>
+        <div className={"alert-content " + alert.alertClassName}>
+          <p>{alert.message}</p>
           <button className="close-btn" onClick={close}>
             &times;
           </button>
@@ -45,7 +45,7 @@ const AlertProvider = props => {
       </div>
 
       {props.children}
-    </AlertContext.Provider>
+    </div>
   );
 };
 
