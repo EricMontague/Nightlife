4ae94f4f9d32ee9fd817eb08e6dbc6d3647eef92
Eric Montague <eric.g.montague@gmail.com>
2020-10-06 00:09:01 -0400

    Started to convert my actionTypes object into separate constants.

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -14,7 +14,6 @@ const actionTypes = Object.freeze({
   planList: {
     GET_PLANS: "GET_PLANS",
     ADD_PLAN: "ADD_PLAN",
-    SET_SELECTED_PLAN: "SET_SELECTED_PLAN",
     DELETE_PLAN: "DELETE_PLAN"
   },
   plan: {
@@ -22,5 +21,23 @@ const actionTypes = Object.freeze({
     SET_PLAN: "SET_PLAN"
   }
 });
+
+// Single Plan
+export const CREATE_PLAN = "CREATE_PLAN";
+export const FETCH_PLAN = "FETCH_PLAN";
+export const UPDATE_PLAN = "UPDATE_PLAN";
+export const RESET_PLAN = "RESET_PLAN";
+
+// Plan list
+export const DELETE_PLAN = "DELETE_PLAN";
+
+// Users
+export const USER_SIGN_IN = "USER_SIGN_IN";
+export const USER_SIGN_OUT = "USER_SIGN_OUT_FAIL";
+
+// Alerts
+export const SHOW_SUCCESS_ALERT = "SHOW_SUCCESS_ALERT";
+export const SHOW_ERROR_ALERT = "SHOW_ERROR_ALERT";
+export const CLEAR_ALERT = "CLEAR_ALERT";
 
 export default actionTypes;
