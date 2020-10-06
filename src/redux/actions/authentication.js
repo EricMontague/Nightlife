7323ccbd6bc89160af91cadb76fac0d362214ae8
Eric Montague <eric.g.montague@gmail.com>
2020-10-06 00:12:20 -0400

    Added functions from the old authentication module in the firebase directory and made them thunks. Also added proper error handling so that they will update the alert reducer if any errors occur.

--- src/redux/actions/authentication.js
+++ src/redux/actions/authentication.js
@@ -1,19 +1,19 @@
-import actionTypes from "./types";
+import { USER_SIGN_IN, USER_SIGN_OUT, SHOW_ERROR_ALERT } from "./types";
 import {
   disablePointerEvents,
   enablePointerEvents
 } from "../../utils/commonHelpers";
 import Poller from "../../utils/polling";
-import { auth } from "../../firebase/firebaseApp";
-import { getUserDocument } from "../../firebase/users";
+import { auth, provider } from "../../firebase/firebaseApp";
+import { getUserDocument, storeUserDocument } from "../../firebase/users";
 
 // Get user from firestore
 const fetchUser = async (userId, dispatch) => {
   const currentUser = await getUserDocument(userId);
   enablePointerEvents();
   dispatch({
-    type: actionTypes.auth.SIGN_IN,
-    currentUser: {
+    type: USER_SIGN_IN,
+    payload: {
       displayName: currentUser.displayName,
       email: currentUser.email,
       photoURL: currentUser.photoURL,
@@ -27,16 +27,15 @@ export const authStateListener = () => dispatch => {
     // user logging out
     if (!userAuth) {
       dispatch({
-        type: actionTypes.auth.SIGN_OUT,
-        currentUser: null
+        type: USER_SIGN_OUT
       });
     } else {
       if (userAuth.displayName) {
         disablePointerEvents();
         // user signs in with google
         dispatch({
-          type: actionTypes.auth.SIGN_IN,
-          currentUser: {
+          type: USER_SIGN_IN,
+          payload: {
             displayName: userAuth.displayName,
             email: userAuth.email,
             photoURL: userAuth.photoURL,
@@ -53,4 +52,88 @@ export const authStateListener = () => dispatch => {
     }
   });
   return listener;
+};
+
+export const registerUser = user => async dispatch => {
+  // Create user account through firebase
+
+  let results;
+  try {
+    results = await auth.createUserWithEmailAndPassword(
+      user.email,
+      user.password
+    );
+    await storeUserDocument({
+      id: results.user.uid,
+      email: user.email,
+      password: user.password,
+      displayName: user.firstName + " " + user.lastName,
+      photoURL: "",
+      plans: []
+    });
+  } catch (error) {
+    dispatch({
+      type: SHOW_ERROR_ALERT,
+      payload: error.message
+    });
+  }
+};
+
+export const signInWithEmailAndPassword = (
+  email,
+  password
+) => async dispatch => {
+  try {
+    return await auth.signInWithEmailAndPassword(email, password);
+  } catch (error) {
+    dispatch({
+      type: SHOW_ERROR_ALERT,
+      payload: error.message
+    });
+  }
+};
+
+export const signOut = () => async dispatch => {
+  try {
+    return await auth.signOut();
+  } catch (error) {
+    dispatch({
+      type: SHOW_ERROR_ALERT,
+      payload: error.message
+    });
+  }
+};
+
+export const signInWithGoogle = () => async dispatch => {
+  try {
+    return await auth.signInWithRedirect(provider);
+  } catch (error) {
+    dispatch({
+      type: SHOW_ERROR_ALERT,
+      payload: error.message
+    });
+  }
+};
+
+export const getRedirectResult = () => async dispatch => {
+  let result;
+  try {
+    result = await auth.getRedirectResult();
+  } catch (error) {
+    dispatch({
+      type: SHOW_ERROR_ALERT,
+      payload: error.message
+    });
+  }
+
+  if (result.user) {
+    storeUserDocument({
+      id: result.user.uid,
+      email: result.user.email,
+      displayName: result.user.displayName,
+      photoURL: result.user.photoURL,
+      plans: []
+    });
+    return result;
+  }
 };
