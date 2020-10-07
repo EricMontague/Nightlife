f033b8cbdf674f857703f7e151ba0d1f7ff36b66
Eric Montague <eric.g.montague@gmail.com>
2020-10-06 20:54:14 -0400

    Moved the functions for storing and updating plans in firestore to this file and made them thunks

--- src/redux/actions/plan.js
+++ src/redux/actions/plan.js
@@ -1,6 +1,14 @@
 import { trimObjectFieldValues } from "../../utils/commonHelpers";
-import { CREATE_PLAN, UPDATE_PLAN } from "./types";
+import {
+  CREATE_PLAN,
+  UPDATE_PLAN,
+  RESET_PLAN,
+  SHOW_SUCCESS_ALERT,
+  SHOW_ERROR_ALERT
+} from "./types";
+import { addPlan, updatePlan } from "../../firebase/plans";
 import { v4 as uuidv4 } from "uuid";
+import constants from "../../utils/constants";
 
 export const addPlanDetails = plan => {
   const trimmedPlan = trimObjectFieldValues(plan);
@@ -19,4 +27,83 @@ export const updatePlanDetails = plan => {
     type: UPDATE_PLAN,
     payload: trimmedPlan
   };
+};
+
+export const resetPlanDetails = () => {
+  return {
+    type: RESET_PLAN
+  };
+};
+
+const stripPlaces = placesList => {
+  return placesList.map((place, index) => {
+    return { placeId: place.placeId, sortKey: index };
+  });
+};
+
+// Google doesn't allow storage of Places API data for more than 30 days,
+// with the sole exception being the the placeId attribute.
+// Based on their terms and conditions, the placeId can be stored indefinitely
+// https://developers.google.com/places/web-service/policies
+export const storePlanInFirestore = (
+  userId,
+  places,
+  plan
+) => async dispatch => {
+  let successful = true;
+  if (places.length === 0) {
+    dispatch({
+      type: SHOW_ERROR_ALERT,
+      payload: constants.NO_PLACES_SELECTED_ERROR_MESSAGE
+    });
+    successful = false;
+  } else {
+    plan.places = stripPlaces(places);
+    try {
+      await addPlan(userId, plan);
+    } catch (error) {
+      dispatch({
+        type: SHOW_ERROR_ALERT,
+        payload: error.message
+      });
+      successful = false;
+    }
+    dispatch({
+      type: SHOW_SUCCESS_ALERT,
+      payload: constants.PLAN_CREATED_SUCCESS_MESSAGE
+    });
+  }
+  return successful;
+};
+
+export const updatePlanInFirestore = (
+  userId,
+  places,
+  plan
+) => async dispatch => {
+  let successful = true;
+  if (places.length === 0) {
+    dispatch({
+      type: SHOW_ERROR_ALERT,
+      payload: constants.NO_PLACES_SELECTED_ERROR_MESSAGE
+    });
+    successful = false;
+  } else {
+    plan.places = stripPlaces(places);
+
+    try {
+      await updatePlan(userId, plan);
+    } catch (error) {
+      dispatch({
+        type: SHOW_ERROR_ALERT,
+        payload: error.message
+      });
+      successful = false;
+    }
+    dispatch({
+      type: SHOW_SUCCESS_ALERT,
+      payload: constants.PLAN_UPDATED_SUCCESS_MESSAGE
+    });
+    return successful;
+  }
 };
