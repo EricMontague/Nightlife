7cd18440b276f633c871d114999c5a0c447a84b7
Eric Montague <eric.g.montague@gmail.com>
2020-10-08 00:48:16 -0400

    Changed the implementation of the rootReducer so that I can reset the state of redux when the user logs out

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,13 +4,21 @@ import planListReducer from "./planListReducer";
 import planReducer from "./planReducer";
 import userReducer from "./userReducer";
 import alertReducer from "./alertReducer";
+import { USER_SIGN_OUT_SUCCESS } from "../actions/types";
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   placeListReducer,
   planListReducer,
   planReducer,
   userReducer,
   alertReducer
 });
+
+const rootReducer = (state, action) => {
+  if (action.type === USER_SIGN_OUT_SUCCESS) {
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
 
 export default rootReducer;
