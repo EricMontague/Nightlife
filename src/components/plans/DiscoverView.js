9a0e5a16cde99dc04a4ad99be1115afe0ba1a216
Eric Montague <eric.g.montague@gmail.com>
2020-10-10 19:20:09 -0400

    Converted into a function component

--- src/components/plans/DiscoverView.js
+++ src/components/plans/DiscoverView.js
@@ -4,17 +4,16 @@ import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import SelectList from "../form_inputs/SelectList";
 import PlaceList from "./PlaceList";
 import AutocompleteInputGroup from "./AutocompleteInputGroup";
+import useDeviceState from "../../hooks/useDeviceState";
 import constants from "../../utils/constants";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-class DiscoverView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleBlur = this.handleBlur.bind(this);
-    this.handleFocus = this.handleFocus.bind(this);
-  }
+const DiscoverView = props => {
+  //Declare Hooks
+  const device = useDeviceState();
 
-  handleBlur(event) {
+  // Declare callbacks
+  const handleBlur = event => {
     if (event.target.value) {
       event.target.classList.remove("input-without-error");
       event.target.nextElementSibling.classList.remove("label-without-error");
@@ -25,100 +24,98 @@ class DiscoverView extends React.Component {
         "label-selected"
       );
     }
-  }
+  };
 
-  handleFocus(event) {
+  const handleFocus = event => {
     event.target.classList.add("input-without-error");
     event.target.nextElementSibling.classList.add("label-without-error");
     event.target.nextElementSibling.classList.add("label-selected");
-  }
+  };
 
-  render() {
-    let title = "Discover Places";
-    if (this.props.discoverMode === constants.DISCOVER_MODE.VIEW) {
-      title = this.props.plan.title;
-    }
-    let actionBtnText = "Finish";
-    if (this.props.discoverMode === constants.DISCOVER_MODE.EDIT) {
-      actionBtnText = "Update";
-    }
-    return (
-      <div className="card">
-        {this.props.discoverMode !== constants.DISCOVER_MODE.VIEW && (
-          <SelectList
-            inputName="sort-by"
-            labelName="Sort By"
-            extraClasses="justify-end"
-            handleChange={this.props.handleSelectListChange}
-            isVertical={false}
-            defaultValue={{ value: "", text: "Choose an option" }}
-            values={[
-              "Rating: low to high",
-              "Rating: high to low",
-              "Price: low to high",
-              "Price: high to low"
-            ]}
+  let title = "Discover Places";
+  if (props.discoverMode === constants.DISCOVER_MODE.VIEW) {
+    title = props.plan.title;
+  }
+  let actionBtnText = "Finish";
+  if (props.discoverMode === constants.DISCOVER_MODE.EDIT) {
+    actionBtnText = "Update";
+  }
+  return (
+    <div className="card">
+      {props.discoverMode !== constants.DISCOVER_MODE.VIEW && (
+        <SelectList
+          inputName="sort-by"
+          labelName="Sort By"
+          extraClasses="justify-end"
+          handleChange={props.handleSelectListChange}
+          isVertical={false}
+          defaultValue={{ value: "", text: "Choose an option" }}
+          values={[
+            "Rating: low to high",
+            "Rating: high to low",
+            "Price: low to high",
+            "Price: high to low"
+          ]}
+        />
+      )}
+      <div className="card-header border-bottom mt-4 pb-2">
+        <h3 className="card-title mb-2">{title}</h3>
+        {props.discoverMode !== constants.DISCOVER_MODE.VIEW && (
+          <AutocompleteInputGroup
+            autoFocus={device.isDesktop}
+            handlePlaceSelected={props.handlePlaceSelected}
+            handleFocus={handleFocus}
+            handleBlur={handleBlur}
+            types={constants.GOOGLE_PLACE_TYPES}
+            fields={constants.PLACES_API_FIELDS}
+            autocompleteClassName="autocomplete"
+            inputName="search"
+            labelName="Search"
           />
         )}
-        <div className="card-header border-bottom mt-4 pb-2">
-          <h3 className="card-title mb-2">{title}</h3>
-          {this.props.discoverMode !== constants.DISCOVER_MODE.VIEW && (
-            <AutocompleteInputGroup
-              autoFocus={true}
-              handlePlaceSelected={this.props.handlePlaceSelected}
-              handleFocus={this.handleFocus}
-              handleBlur={this.handleBlur}
-              types={constants.GOOGLE_PLACE_TYPES}
-              fields={constants.PLACES_API_FIELDS}
-              autocompleteClassName="autocomplete"
-              inputName="search"
-              labelName="Search"
-            />
-          )}
-        </div>
-        <DragDropContext onDragEnd={this.props.dragEndHandler}>
-          <Droppable droppableId={"droppable-1"}>
-            {provided => (
-              <div ref={provided.innerRef} {...provided.droppableProps}>
-                <PlaceList
-                  toggleModal={this.props.toggleModal}
-                  places={this.props.places}
-                  handleDeleteClick={this.props.handleDeleteClick}
-                  discoverMode={this.props.discoverMode}
-                  mousedOverPlaceId={this.props.mousedOverPlaceId}
-                />
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
-        {this.props.discoverMode !== constants.DISCOVER_MODE.VIEW && (
-          <>
-            <button
-              type="button"
-              className="btn btn-medium back-btn"
-              onClick={this.props.handleBackClick}
-            >
-              <FontAwesomeIcon icon={["fa", "chevron-left"]} />
-              Back
-            </button>
-            <button
-              type="button"
-              className="btn btn-primary float-right"
-              onClick={() => {
-                return this.props.discoverMode === constants.DISCOVER_MODE.EDIT
-                  ? this.props.handleUpdateClick()
-                  : this.props.handleFinishClick();
-              }}
-            >
-              {actionBtnText}
-            </button>
-          </>
-        )}
       </div>
-    );
-  }
-}
+      <DragDropContext onDragEnd={props.dragEndHandler}>
+        <Droppable droppableId={"droppable-1"}>
+          {provided => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              <PlaceList
+                toggleModal={props.toggleModal}
+                places={props.places}
+                handleDeleteClick={props.handleDeleteClick}
+                discoverMode={props.discoverMode}
+                mousedOverPlaceId={props.mousedOverPlaceId}
+              />
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+      {props.discoverMode !== constants.DISCOVER_MODE.VIEW && (
+        <>
+          <button
+            type="button"
+            className="btn btn-medium back-btn"
+            onClick={props.handleBackClick}
+          >
+            <FontAwesomeIcon icon={["fa", "chevron-left"]} />
+            Back
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary float-right"
+            onClick={() => {
+              return props.discoverMode === constants.DISCOVER_MODE.EDIT
+                ? props.handleUpdateClick()
+                : props.handleFinishClick();
+            }}
+          >
+            {actionBtnText}
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
 
 DiscoverView.propTypes = {
   handlePlaceSelected: PropTypes.func.isRequired,
